a8707912bb26407e15523dc10a03e4d4b0f7b8ef
Ilya Obukhov <ilya.obukhov.bs@gmail.com>
2025-02-12 15:01:41 +0100

    Switch to using a central TOML file for dependency versions
    Replaced hardcoded dependency versions in build files with references from a centralized `libs.versions.toml` file. This improves maintainability and consistency by consolidating version management in one place. Removed redundant properties from `gradle.properties` and updated plugin and dependency declarations accordingly.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "2.1.10"
-    id("io.ktor.plugin") version "3.0.3"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 group = "com.example"
@@ -22,13 +22,14 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-content-negotiation")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("io.ktor:ktor-server-websockets")
-    implementation("io.ktor:ktor-server-netty")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml")
-    testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.websockets)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback)
+    implementation(libs.ktor.server.config.yaml)
+
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test)
 }
