a0e5ab6280e354d0c85cd741291409090e704e2b
Ilya Obukhov <ilya.obukhov.bs@gmail.com>
2025-02-18 17:15:05 +0100

    Add Milvus integration for embedding storage and retrieval
    Introduced Milvus-based embedding functionality with endpoints for inserting documents and searching content. Updated dependencies, created related tests, and included Milvus-specific configurations in Docker. Removed obsolete Ollama test code to streamline the project.

--- src/main/kotlin/chat/endpoints.kt
+++ src/main/kotlin/chat/endpoints.kt
@@ -10,10 +10,13 @@ import dev.langchain4j.data.segment.TextSegment
 import dev.langchain4j.memory.chat.MessageWindowChatMemory
 import dev.langchain4j.model.chat.chat
 import dev.langchain4j.model.chat.request.DefaultChatRequestParameters
+import dev.langchain4j.model.ollama.OllamaEmbeddingModel
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever
 import dev.langchain4j.service.AiServices
+import dev.langchain4j.store.embedding.EmbeddingStore
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore
+import dev.langchain4j.store.embedding.milvus.MilvusEmbeddingStore
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -22,10 +25,16 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.serialization.Serializable
 
+
 @Serializable
 data class Message(
     val chatId: Int = 0,
     val message: String = "",
+)
+
+@Serializable
+data class Document(
+    val content: String = "",
 )
 
 fun Route.chat() {
@@ -62,6 +71,54 @@ fun Route.chat() {
             .chatLanguageModel(Llama32.model)
             .chatMemoryProvider { _ -> MessageWindowChatMemory.withMaxMessages(10) }
             .contentRetriever(EmbeddingStoreContentRetriever.from(store))
+            .build()
+
+        val answer = instance.documentation(message.chatId, message.message)
+        call.respondText { answer }
+    }
+
+    post("/milvus/insert") {
+        val document = call.receive<Document>()
+
+        val embeddingStore: EmbeddingStore<TextSegment> = MilvusEmbeddingStore.builder()
+            .uri("http://127.0.0.1:19530")
+            .collectionName("animals_collection_8")
+            .dimension(768)
+            .build()
+
+        val embeddingModel = OllamaEmbeddingModel.builder()
+            .baseUrl("http://localhost:11434")
+            .modelName("nomic-embed-text:latest")
+            .build()
+
+        val embedding = embeddingModel.embed(document.content).content()
+        val result = embeddingStore.add(embedding, TextSegment.from(document.content))
+        call.respondText { result }
+
+    }
+
+    post("/milvus/search") {
+        val message = call.receive<Message>()
+
+        val embeddingStore: EmbeddingStore<TextSegment> = MilvusEmbeddingStore.builder()
+            .uri("http://127.0.0.1:19530")
+            .collectionName("animals_collection_8")
+            .build()
+
+        val embeddingModel = OllamaEmbeddingModel.builder()
+            .baseUrl("http://localhost:11434")
+            .modelName("nomic-embed-text:latest")
+            .build()
+
+        val contentRetriever = EmbeddingStoreContentRetriever.builder()
+            .embeddingStore(embeddingStore)
+            .embeddingModel(embeddingModel)
+            .build()
+
+        val instance: Chat = AiServices.builder(Chat::class.java)
+            .chatLanguageModel(Llama32.model)
+            .chatMemoryProvider { _ -> MessageWindowChatMemory.withMaxMessages(10) }
+            .contentRetriever(contentRetriever)
             .build()
 
         val answer = instance.documentation(message.chatId, message.message)
---
+++ src/test/kotlin/Milvus.kt
@@ -0,0 +1,37 @@
+package com.example
+
+import com.example.chat.Document
+import com.example.chat.Message
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+
+class Milvus {
+
+    @Test
+    fun documentInsertTest() = testApplication {
+        application { module() }
+        val client = createClient { install(ContentNegotiation) { json() } }
+
+        client.post("milvus/insert") {
+            contentType(ContentType.Application.Json)
+            setBody(Document("Squirrel named Squeaky have brown and triangle shape."))
+        }
+    }
+
+    @Test
+    fun messageSearchTest() = testApplication {
+        application { module() }
+        val client = createClient { install(ContentNegotiation) { json() } }
+
+        val response = client.post("milvus/search") {
+            contentType(ContentType.Application.Json)
+            setBody(Message(1, "What animal is Squeaky, and what shape and color is it?"))
+        }
+        println(response.bodyAsText())
+    }
+}
